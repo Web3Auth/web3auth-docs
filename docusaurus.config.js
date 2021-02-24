2c3858c6c7f0b9d7c5b98d5cce094e47d5471756
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-02-24 16:54:09 +0700

    Docs-only mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,6 +84,7 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
