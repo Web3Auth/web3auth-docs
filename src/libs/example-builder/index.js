de54a9ef6619513fdd9bae48fc906c0609d9dd41
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-03 14:58:06 +0700

    Torus Wallet content

--- src/libs/example-builder/index.js
+++ src/libs/example-builder/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import * as DirectAuth from "./direct-auth";
+import * as TorusWallet from "./torus-wallet";
 
-export default function buildExample() {
-  return (
-    <div>
-      <DirectAuth.InstallWebSDK />
-      <DirectAuth.ServeServiceWorker />
-      <DirectAuth.ServeRedirectPage />
-      <DirectAuth.InstantiateSDKInstance />
-      <DirectAuth.TriggerLogin />
-    </div>
-  );
+export default function buildExample({ product }) {
+  switch (product.displayName) {
+    case "DirectAuth":
+      return (
+        <div>
+          <DirectAuth.InstallWebSDK />
+          <DirectAuth.ServeServiceWorker />
+          <DirectAuth.ServeRedirectPage />
+          <DirectAuth.InstantiateSDKInstance />
+          <DirectAuth.TriggerLogin />
+        </div>
+      );
+    case "Torus Wallet":
+      return (
+        <div>
+          <TorusWallet.InstallWebSDK />
+          <TorusWallet.InstantiateSDKInstance />
+          <TorusWallet.TriggerLogin />
+          <TorusWallet.IntegrateWithWeb3 />
+        </div>
+      );
+  }
 }
