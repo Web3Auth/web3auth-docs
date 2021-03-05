706fd5c6732f636bcb62500568434cf54325333c
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-05 13:14:32 +0700

    Remove redundant code

--- src/libs/example-builder/direct-auth/index.js
+++
@@ -1,5 +0,0 @@
-export { default as InstallWebSDK } from "./install-web-sdk.component.mdx";
-export { default as ServeServiceWorker } from "./serve-service-worker.component.mdx";
-export { default as ServeRedirectPage } from "./serve-redirect-page.component.mdx";
-export { default as InstantiateSDKInstance } from "./instantiate-sdk-instance.component.mdx";
-export { default as TriggerLogin } from "./trigger-login.component.mdx";
