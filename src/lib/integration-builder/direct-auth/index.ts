6a1430330e7be9fbc0ad74608437f2ad2349b3f9
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-15 16:16:16 +0700

    Add Android example

--- src/lib/integration-builder/direct-auth/index.ts
+++ src/lib/integration-builder/direct-auth/index.ts
@@ -13,7 +13,7 @@ const directAuthIntegrationBuilder: IntegrationBuilder = {
     lang: {
       displayName: "Language/Framework",
       default: "React",
-      choices: ["React", "Vue"],
+      choices: ["React", "Vue", "Android"],
     },
   },
 
@@ -21,9 +21,11 @@ const directAuthIntegrationBuilder: IntegrationBuilder = {
     const availableOptions: Record<string, string>[] = [];
     switch (optionKey) {
       case "chain":
-        availableOptions.push({ lang: "React" }, { lang: "Vue" });
-        // if (optionValue === "Ethereum")
-        //   availableOptions.push({ lang: "iOS" }, { lang: "Android" });
+        availableOptions.push(
+          { lang: "React" },
+          { lang: "Vue" },
+          { lang: "Android" }
+        );
         break;
       case "lang":
         availableOptions.push({ chain: "Ethereum" });
@@ -40,57 +42,93 @@ const directAuthIntegrationBuilder: IntegrationBuilder = {
     const filenames: string[] = [];
     const steps: IntegrationStep[] = [];
 
-    if (values.lang === "React") {
-      filenames.push("react/App.js", "react/index.js");
-      steps.push(
-        {
-          ...STEPS.installWebSDK,
-          pointer: { filename: "react/App.js", range: "2" },
-        },
-        {
-          ...STEPS.instantiateWebSDK,
-          pointer: { filename: "react/App.js", range: "137-143" },
-        },
-        {
-          ...STEPS.serveWebSw,
-          pointer: { filename: "web/sw.js" },
-        },
-        {
-          ...STEPS.serveWebRedirect,
-          pointer: { filename: "web/redirect.html" },
-        },
-        {
-          ...STEPS.triggerWebLogin,
-          pointer: { filename: "react/App.js", range: "158-163" },
-        }
+    if (values.lang === "React" || values.lang === "Vue") {
+      if (values.lang === "React") {
+        filenames.push("react/App.js", "react/index.js");
+        steps.push(
+          {
+            ...STEPS.installWebSDK,
+            pointer: { filename: "react/App.js", range: "2" },
+          },
+          {
+            ...STEPS.instantiateWebSDK,
+            pointer: { filename: "react/App.js", range: "137-143" },
+          },
+          {
+            ...STEPS.serveWebSw,
+            pointer: { filename: "web/sw.js" },
+          },
+          {
+            ...STEPS.serveWebRedirect,
+            pointer: { filename: "web/redirect.html" },
+          },
+          {
+            ...STEPS.triggerWebLogin,
+            pointer: { filename: "react/App.js", range: "158-163" },
+          }
+        );
+      } else {
+        filenames.push("vue/App.vue", "vue/main.js");
+        steps.push(
+          {
+            ...STEPS.installWebSDK,
+            pointer: { filename: "vue/App.vue", range: "50" },
+          },
+          {
+            ...STEPS.instantiateWebSDK,
+            pointer: { filename: "vue/App.vue", range: "272-277" },
+          },
+          {
+            ...STEPS.serveWebSw,
+            pointer: { filename: "web/sw.js" },
+          },
+          {
+            ...STEPS.serveWebRedirect,
+            pointer: { filename: "web/redirect.html" },
+          },
+          {
+            ...STEPS.triggerWebLogin,
+            pointer: { filename: "vue/App.vue", range: "210-217" },
+          }
+        );
+      }
+
+      filenames.push("web/sw.js", "web/redirect.html");
+    } else if (values.lang === "Android") {
+      filenames.push(
+        "android/build.gradle",
+        "android/app/build.gradle",
+        "android/app/MainActivity.java"
       );
-    } else if (values.lang === "Vue") {
-      filenames.push("vue/App.vue", "vue/main.js");
       steps.push(
         {
-          ...STEPS.installWebSDK,
-          pointer: { filename: "vue/App.vue", range: "50" },
+          ...STEPS.addAndroidJitpack,
+          pointer: { filename: "android/build.gradle", range: "17" },
         },
         {
-          ...STEPS.instantiateWebSDK,
-          pointer: { filename: "vue/App.vue", range: "272-277" },
+          ...STEPS.installAndroidSDK,
+          pointer: { filename: "android/app/build.gradle", range: "50" },
         },
         {
-          ...STEPS.serveWebSw,
-          pointer: { filename: "web/sw.js" },
+          ...STEPS.registerAndroidRedirect,
+          pointer: { filename: "android/app/build.gradle", range: "16-20" },
         },
         {
-          ...STEPS.serveWebRedirect,
-          pointer: { filename: "web/redirect.html" },
+          ...STEPS.instantiateAndroidSDK,
+          pointer: {
+            filename: "android/app/MainActivity.java",
+            range: "56-57",
+          },
         },
         {
-          ...STEPS.triggerWebLogin,
-          pointer: { filename: "vue/App.vue", range: "210-217" },
+          ...STEPS.triggerAndroidLogin,
+          pointer: {
+            filename: "android/app/MainActivity.java",
+            range: "74-83",
+          },
         }
       );
     }
-
-    filenames.push("web/sw.js", "web/redirect.html");
 
     return {
       filenames: filenames.map((it) => `direct-auth/${it}`),
--- src/lib/integration-builder/direct-auth/steps/index.ts
+++ src/lib/integration-builder/direct-auth/steps/index.ts
@@ -1,16 +1,33 @@
+// Web
 import * as installWebSDK from "./install-web-sdk.mdx";
 import * as instantiateWebSDK from "./instantiate-web-sdk.mdx";
 import * as serveWebSw from "./serve-web-sw.mdx";
 import * as serveWebRedirect from "./serve-web-redirect.mdx";
 import * as triggerWebLogin from "./trigger-web-login.mdx";
+
+// Android
+import * as addAndroidJitpack from "./add-android-jitpack.mdx";
+import * as installAndroidSDK from "./install-android-sdk.mdx";
+import * as registerAndroidRedirect from "./register-android-redirect.mdx";
+import * as instantiateAndroidSDK from "./instantiate-android-sdk.mdx";
+import * as triggerAndroidLogin from "./trigger-android-login.mdx";
+
 import { toSteps } from "../../utils";
 
 const STEPS = toSteps({
+  // Web
   installWebSDK,
   instantiateWebSDK,
   serveWebSw,
   serveWebRedirect,
   triggerWebLogin,
+
+  // Android
+  addAndroidJitpack,
+  installAndroidSDK,
+  registerAndroidRedirect,
+  instantiateAndroidSDK,
+  triggerAndroidLogin,
 });
 
 export default STEPS;
