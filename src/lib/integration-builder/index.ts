3f73c4b83d5df1eb2eecdb801d9a1510ab21d87e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-16 16:20:04 +0700

    Add examples

--- src/lib/integration-builder/index.ts
+++ src/lib/integration-builder/index.ts
@@ -1,10 +1,12 @@
 import { IntegrationBuilder } from "./interfaces";
 import directAuthIntegrationBuilder from "./direct-auth";
 import torusWalletIntegrationBuilder from "./torus-wallet";
+import openLoginIntegrationBuilder from "./open-login";
 
 const integrationBuilders: Record<string, IntegrationBuilder> = {
   "torus-wallet": torusWalletIntegrationBuilder,
   "direct-auth": directAuthIntegrationBuilder,
+  "open-login": openLoginIntegrationBuilder,
 };
 
 export * from "./interfaces";
