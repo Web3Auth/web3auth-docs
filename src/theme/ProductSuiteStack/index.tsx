5c65257e66d02bb8aa7a133c04f0dd207e459447
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-21 15:52:46 +0700

    Set tab state

--- src/theme/ProductSuiteStack/index.tsx
+++ src/theme/ProductSuiteStack/index.tsx
@@ -1,36 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 
 export default function ProductSuiteStack() {
+  const [tab, setTab] = useState<
+    "overview" | "openlogin" | "wallet" | "custom-auth" | "network"
+  >("overview");
+
+  let content: JSX.Element;
+  switch (tab) {
+    case "overview":
+      content = <div>Overview</div>;
+      break;
+    case "openlogin":
+      content = <div>OpenLogin</div>;
+      break;
+    case "wallet":
+      content = <div>Wallet</div>;
+      break;
+    case "custom-auth":
+      content = <div>CustomAuth</div>;
+      break;
+    case "network":
+      content = <div>Network</div>;
+      break;
+    default:
+      content = <></>;
+      break;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.tabsContainer}>
         <div className={styles.tabSection}>
           <ul>
-            <li className={styles.selected}>Overview</li>
+            <li
+              className={tab === "overview" && styles.selected}
+              onClick={() => setTab("overview")}
+            >
+              Overview
+            </li>
           </ul>
         </div>
         <div className={styles.tabSection}>
           <span>Plug n play</span>
           <ul>
-            <li>OpenLogin</li>
-            <li>Wallet</li>
+            <li
+              className={tab === "openlogin" && styles.selected}
+              onClick={() => setTab("openlogin")}
+            >
+              OpenLogin
+            </li>
+            <li
+              className={tab === "wallet" && styles.selected}
+              onClick={() => setTab("wallet")}
+            >
+              Wallet
+            </li>
           </ul>
         </div>
         <div className={styles.tabSection}>
           <span>Full customization</span>
           <ul>
-            <li>CustomAuth</li>
+            <li
+              className={tab === "custom-auth" && styles.selected}
+              onClick={() => setTab("custom-auth")}
+            >
+              CustomAuth
+            </li>
           </ul>
         </div>
         <div className={styles.tabSection}>
           <span>Infrastructure</span>
           <ul>
-            <li>Network</li>
+            <li
+              className={tab === "network" && styles.selected}
+              onClick={() => setTab("network")}
+            >
+              Network
+            </li>
           </ul>
         </div>
       </div>
-      <div className={styles.contentContainer}>content</div>
+      <div className={styles.contentContainer}>{content}</div>
     </div>
   );
 }
