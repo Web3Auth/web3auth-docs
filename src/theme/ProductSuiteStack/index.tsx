e7b033e56fdbb4fff10a2d10b2cf813d2f811573
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-21 15:30:13 +0700

    Add tab layout

--- src/theme/ProductSuiteStack/index.tsx
+++ src/theme/ProductSuiteStack/index.tsx
@@ -4,7 +4,32 @@ import styles from "./styles.module.css";
 export default function ProductSuiteStack() {
   return (
     <div className={styles.container}>
-      <div className={styles.tabsContainer}>tabs</div>
+      <div className={styles.tabsContainer}>
+        <div className={styles.tabSection}>
+          <ul>
+            <li className={styles.selected}>Overview</li>
+          </ul>
+        </div>
+        <div className={styles.tabSection}>
+          <span>Plug n play</span>
+          <ul>
+            <li>OpenLogin</li>
+            <li>Wallet</li>
+          </ul>
+        </div>
+        <div className={styles.tabSection}>
+          <span>Full customization</span>
+          <ul>
+            <li>CustomAuth</li>
+          </ul>
+        </div>
+        <div className={styles.tabSection}>
+          <span>Infrastructure</span>
+          <ul>
+            <li>Network</li>
+          </ul>
+        </div>
+      </div>
       <div className={styles.contentContainer}>content</div>
     </div>
   );
