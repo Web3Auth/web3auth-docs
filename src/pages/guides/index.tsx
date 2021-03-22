cbcbe444aef8f2252467bd7c7f3563a9d2a04164
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-22 11:01:39 +0700

    Add guides page

---
+++ src/pages/guides/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function GuidesPage() {
+  return (
+    <Layout title="Guides">
+      <div>Guides</div>
+    </Layout>
+  );
+}
