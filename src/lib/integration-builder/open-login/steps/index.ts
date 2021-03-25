0d5e7970a693d14419d1c92cb17c425b47dec86f
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-25 17:11:30 +0700

    Add OpenLogin interactive example

--- src/lib/integration-builder/open-login/steps/index.ts
+++ src/lib/integration-builder/open-login/steps/index.ts
@@ -1,14 +1,20 @@
-import * as step1 from "./step-1.mdx";
-import * as step2 from "./step-2.mdx";
-import * as step3 from "./step-3.mdx";
-import * as step4 from "./step-4.mdx";
+import * as installSDK from "./install-sdk.mdx";
+import * as registerApp from "./register-app.mdx";
+import * as instantiateSDK from "./instantiate-sdk.mdx";
+import * as retrievePrivateKey from "./retrieve-private-key.mdx";
+import * as triggerLogin from "./trigger-login.mdx";
+import * as connectWithWeb3 from "./connect-with-web3.mdx";
+import * as logout from "./logout.mdx";
 import { toSteps } from "../../utils";
 
 const STEPS = toSteps({
-  step1,
-  step2,
-  step3,
-  step4,
+  installSDK,
+  registerApp,
+  instantiateSDK,
+  retrievePrivateKey,
+  triggerLogin,
+  connectWithWeb3,
+  logout,
 });
 
 export default STEPS;
