e75936d4e3b6cff3045d4c18bf6a177859e906c0
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-09 10:51:18 +0700

    Add whitelabel instructions to integration builders

--- src/lib/integration-builder/wallet/steps/index.ts
+++ src/lib/integration-builder/wallet/steps/index.ts
@@ -1,6 +1,8 @@
 import * as installSDK from "./install-sdk.mdx";
 import * as installSDKWithScriptTags from "./install-sdk-with-script-tags.mdx";
 import * as instantiateSDK from "./instantiate-sdk.mdx";
+import * as customizeLoginProviders from "./customize-login-providers.mdx";
+import * as whitelabel from "./whitelabel.mdx";
 import * as triggerLogin from "./trigger-login.mdx";
 import * as getUserInfo from "./get-user-info.mdx";
 import * as integrateWithWeb3 from "./integrate-with-web3.mdx";
@@ -11,6 +13,8 @@ const STEPS = toSteps({
   installSDK,
   installSDKWithScriptTags,
   instantiateSDK,
+  customizeLoginProviders,
+  whitelabel,
   triggerLogin,
   getUserInfo,
   integrateWithWeb3,
