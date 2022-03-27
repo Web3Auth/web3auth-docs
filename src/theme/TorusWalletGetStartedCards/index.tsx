a47d82e216451cfce3b78d427c46948cbf84c719
Zhen Yu Yong <fuz3zy@gmail.com>
2022-03-27 12:07:36 +0800

    renamed to quick start

--- src/theme/TorusWalletGetStartedCards/index.tsx
+++ src/theme/TorusWalletGetStartedCards/index.tsx
@@ -6,14 +6,14 @@ import styles from "./styles.module.css";
 export default function TorusWalletGetStartedCards() {
   return (
     <div className={styles.container}>
-      <Link className={classNames(styles.card, styles.cardOne)} to="/integration-builder/?b=wallet&lang=HTML">
+      <Link className={classNames(styles.card, styles.cardOne)} to="/quick-start/?b=wallet&lang=HTML">
         <p className={styles.headline}>SSO logins (e.g. Google, email passwordless) connected to Ethereum on the Web</p>
         <p className={styles.poweredBy}>
           <span>Powered by</span>
           <img src="/images/wallet-full-logo.svg" alt="Wallet" />
         </p>
       </Link>
-      <Link className={classNames(styles.card, styles.cardTwo)} to="/integration-builder/?b=wallet&lang=React">
+      <Link className={classNames(styles.card, styles.cardTwo)} to="/quick-start/?b=wallet&lang=React">
         <p className={styles.headline}>Integrate Wallet with your favorite web framework: React or Vue</p>
         <p className={styles.poweredBy}>
           <span>Powered by</span>
