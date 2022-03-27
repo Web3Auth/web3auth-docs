303ea7c01470e89cf26176b184352b4beb914027
Zhen Yu Yong <fuz3zy@gmail.com>
2022-03-27 11:57:29 +0800

    changed default, removed wallet section

--- src/lib/integration-builder/index.ts
+++ src/lib/integration-builder/index.ts
@@ -1,11 +1,9 @@
 import directAuthIntegrationBuilder from "./customauth";
 import { IntegrationBuilder } from "./interfaces";
 import plugnplayIntegrationBuilder from "./plugnplay";
-import torusWalletIntegrationBuilder from "./wallet";
 
 const integrationBuilders: Record<string, IntegrationBuilder> = {
   plugnplay: plugnplayIntegrationBuilder,
-  wallet: torusWalletIntegrationBuilder,
   customauth: directAuthIntegrationBuilder,
 };
 
