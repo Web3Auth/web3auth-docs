374b08abb2304d34551006d3fe64335ad6193f94
Zhen Yu Yong <fuz3zy@gmail.com>
2022-03-28 18:19:25 +0800

    added some steps

--- src/lib/integration-builder/web3auth/steps/index.ts
+++ src/lib/integration-builder/web3auth/steps/index.ts
@@ -1,9 +1,19 @@
 import { toSteps } from "../../utils";
 // web
 import * as installationWeb from "./web/installation.mdx";
+import * as registerApp from "./web/register-app.mdx";
+import * as instantiate from "./web/instantiateSDK.mdx";
+import * as subscribe from "./web/subscribe.mdx";
+import * as initialize from "./web/initializing.mdx";
+import * as triggeringLogin from "./web/triggering-login.mdx";
 
 const STEPS = toSteps({
   installationWeb,
+  registerApp,
+  instantiate,
+  subscribe,
+  initialize,
+  triggeringLogin,
 });
 
 export default STEPS;
