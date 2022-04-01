e7f21f2eb84897459f760255d159b4e60c2de8f8
chaitanyapotti <chaitanya.potti@gmail.com>
2022-04-01 14:29:42 +0800

    html w/o filters

--- src/lib/integration-builder/web3auth/steps/frameworks/html/index.ts
+++ src/lib/integration-builder/web3auth/steps/frameworks/html/index.ts
@@ -1,8 +1,10 @@
 import { toSteps } from "../../../../utils";
+import * as getUserInfo from "./get-user-info.mdx";
 import * as initialize from "./initializing.mdx";
 // web
 import * as installationWeb from "./installation.mdx";
 import * as instantiate from "./instantiateSDK.mdx";
+import * as logout from "./logout.mdx";
 import * as registerApp from "./register-app.mdx";
 import * as subscribe from "./subscribe.mdx";
 import * as triggeringLogin from "./triggering-login.mdx";
@@ -14,6 +16,8 @@ const STEPS = toSteps({
   subscribe,
   initialize,
   triggeringLogin,
+  getUserInfo,
+  logout,
 });
 
 export default STEPS;
