8ea5df27750e6c4da1e3b297b7a6e362ff929261
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-04-04 13:04:06 +0530

    UI Changes in Navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,7 @@ const githubOrg = "web3auth";
 const githubRepo = "web3auth-docs";
 const githubOrgUrl = `https://github.com/${githubOrg}`;
 const githubRepoUrl = `${githubOrgUrl}/${githubRepo}`;
+const githubCoreRepoUrl = `${githubOrgUrl}/${githubOrg}`;
 const githubEditUrl = `${githubRepoUrl}/edit/master`;
 const contactUrl = "https://web3auth.io/contact-us.html"; // TODO: Confirm with content team
 const mediumUrl = "https://medium.com/toruslabs";
@@ -52,131 +53,18 @@ module.exports = {
           className: "navbar__button",
         },
         {
-          label: "Contact us",
+          label: "Schedule a Demo",
           href: contactUrl,
           position: "right",
         },
-      ],
-    },
-    footer: {
-      style: "light",
-      links: [
         {
-          title: "Users",
-          items: [
-            {
-              label: "Torus Wallet",
-              href: "https://app.tor.us",
-            },
-            {
-              label: "OpenLogin",
-              href: "https://app.openlogin.com",
-            },
-          ],
-        },
-        {
-          title: "Developers",
-          items: [
-            {
-              label: "Torus Wallet",
-              href: "https://toruswallet.io/",
-            },
-            {
-              label: "CustomAuth",
-              href: "https://customauth.io",
-            },
-            {
-              label: "OpenLogin",
-              href: "https://openlogin.com",
-            },
-            {
-              label: "tKey",
-              href: "https://tkey.io/",
-            },
-            {
-              label: "Status",
-              href: "https://status.web3auth.io",
-            },
-            {
-              label: "Support",
-              href: "https://t.me/web3authdev",
-            },
-          ],
-        },
-        {
-          // TODO: Confirm with content team
-          title: "Community",
-          items: [
-            {
-              label: "Telegram",
-              href: "https://t.me/web3auth",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/web3auth",
-            },
-            {
-              label: "Medium",
-              href: mediumUrl,
-            },
-            {
-              label: "Github",
-              href: githubOrgUrl,
-            },
-            {
-              label: "Reddit",
-              href: "https://www.reddit.com/r/web3auth",
-            },
-            {
-              label: "LinkedIn",
-              href: "https://www.linkedin.com/company/web3auth",
-            },
-            {
-              label: "YouTube",
-              href: "https://www.youtube.com/c/web3auth",
-            },
-          ],
-        },
-        {
-          // TODO: Confirm with content team
-          title: "Company",
-          items: [
-            {
-              label: "About us",
-              to: "https://web3auth.io/about-us.html",
-            },
-            {
-              label: "Partners",
-              href: "https://web3auth.io/partners.html",
-            },
-            {
-              label: "Career",
-              href: "https://angel.co/company/torus-2",
-            },
-            {
-              label: "Media kit",
-              href: "https://web3auth.io/media-kit.html",
-            },
-            {
-              label: "Blog",
-              href: mediumUrl,
-            },
-            {
-              label: "Contact us",
-              href: contactUrl,
-            },
-          ],
+          position: "right",
+          href: githubCoreRepoUrl,
+          className: "navbar-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
-      logo: {
-        alt: "Web3Auth's Logo",
-        src: "images/logo.svg",
-      },
-      copyright: `© ${new Date().getFullYear()} Torus Labs Private Limited`,
     },
-    // gtag: {
-    //   trackingID: "UA-126622802-2",
-    // },
     algolia: {
       appId: "6OF28D8CMV",
       apiKey: "425a1e860cb4b9b4ce1f7d9b117c7a81",
@@ -194,17 +82,10 @@ module.exports = {
           remarkPlugins: [remarkMath],
           rehypePlugins: [[rehypeKatex, { strict: false }]],
         },
-        /** Uncomment to enable Blog features, see https://v2.docusaurus.io/docs/blog
-        blog: {
-          showReadingTime: true,
-          editUrl: githubEditUrl,
-        },
-        */
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          //
           trackingID: "GTM-ML3T5M6",
         },
       },
