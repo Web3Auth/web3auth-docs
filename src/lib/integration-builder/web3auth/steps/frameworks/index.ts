a657acd4ec0d4512cbbb912efc61cb3685ee33c5
michaellee8 <ckmichael8@gmail.com>
2022-04-08 08:46:23 +0800

    chore: wip

--- src/lib/integration-builder/web3auth/steps/frameworks/index.ts
+++ src/lib/integration-builder/web3auth/steps/frameworks/index.ts
@@ -2,11 +2,13 @@
 import html from "./html";
 import react from "./react";
 import vue from "./vue";
+import ios from "./ios";
 
 const STEPS = {
   html,
   react,
   vue,
+  ios,
 };
 
 export default STEPS;
--- src/lib/integration-builder/web3auth/steps/frameworks/ios/index.ts
+++ src/lib/integration-builder/web3auth/steps/frameworks/ios/index.ts
@@ -1,1 +1,51 @@
 import { replaceFileVariable, toSteps } from "../../../../utils";
+
+import * as installationIOS from "./installation.mdx";
+import * as registerApp from "./register-app.mdx";
+import * as instantiate from "./instantiateSDK.mdx";
+import * as triggeringLogin from "./triggering-login.mdx";
+import * as loginWithJwt from "./login-with-jwt.mdx";
+import * as getUserInfo from "./get-user-info.mdx";
+import * as whiteLabeling from "../common/whitelabeling.mdx";
+
+const STEPS = toSteps({
+  installationIOS,
+  registerApp,
+  instantiate,
+  triggeringLogin,
+  loginWithJwt,
+  getUserInfo,
+  whiteLabeling,
+});
+
+const reactSteps = {
+  STEPS,
+  build({ filenames, files, steps, whitelabel, customAuthentication, customLogin, chain }) {
+    filenames.push("web3auth/ios/Web3Auth.plist");
+    filenames.push("web3auth/ios/ContentView.swift");
+    steps.push(
+      {
+        ...STEPS.installationIOS,
+      },
+      {
+        ...STEPS.registerApp,
+      },
+      {
+        ...STEPS.instantiate,
+      },
+      customAuthentication === "yes"
+        ? {
+            ...STEPS.loginWithJwt,
+          }
+        : {
+            ...STEPS.triggeringLogin,
+          },
+      {
+        ...STEPS.getUserInfo,
+      }
+    );
+    return { filenames, files, steps };
+  },
+};
+
+export default reactSteps;
