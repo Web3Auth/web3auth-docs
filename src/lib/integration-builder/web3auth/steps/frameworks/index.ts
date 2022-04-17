9117959f8503a15ed0b1f65b98f6c5683b7ebfe1
Tai Nguyen <tai.nguyen@tor.us>
2022-04-17 15:00:17 +0700

    add integration builder for angular

--- src/lib/integration-builder/web3auth/steps/frameworks/index.ts
+++ src/lib/integration-builder/web3auth/steps/frameworks/index.ts
@@ -1,4 +1,5 @@
 // web
+import angular from "./angular";
 import html from "./html";
 import react from "./react";
 import vue from "./vue";
@@ -7,6 +8,7 @@ const STEPS = {
   html,
   react,
   vue,
+  angular,
 };
 
 export default STEPS;
