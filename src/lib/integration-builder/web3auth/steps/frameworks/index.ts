5107238808b5554295260d921bf0db5c3caa6bf6
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-04-18 21:28:57 +0200

    Fix build issue

--- src/lib/integration-builder/web3auth/steps/frameworks/index.ts
+++ src/lib/integration-builder/web3auth/steps/frameworks/index.ts
@@ -5,8 +5,8 @@ import next from "./next";
 import react from "./react";
 import vue from "./vue";
 import ios from "./ios";
-import android from "./android"
-import flutter from "./flutter"
+import android from "./android";
+import flutter from "./flutter";
 
 const STEPS = {
   html,
@@ -15,7 +15,7 @@ const STEPS = {
   vue,
   ios,
   android,
-  flutter
+  flutter,
   angular,
 };
 
