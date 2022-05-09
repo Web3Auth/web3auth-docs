3ade3a913aaf4ee984cf4fce67333f0622abff20
Zhen Yu Yong <fuz3zy@gmail.com>
2022-05-09 18:32:26 +0800

    checkpoint

--- src/lib/integration-builder/index.ts
+++ src/lib/integration-builder/index.ts
@@ -4,5 +4,5 @@ import web3authIntegrationBuilder from "./web3auth";
 const integrationBuilder: IntegrationBuilder = web3authIntegrationBuilder;
 
 export * from "./interfaces";
-
+export * from "./web3auth";
 export default integrationBuilder;
