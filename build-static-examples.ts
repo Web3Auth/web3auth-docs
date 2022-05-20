8f7b5112cce69fcaa3c7a2267802e294f65cf942
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-05-21 03:14:25 +0530

    Update build-static-examples.ts

--- build-static-examples.ts
+++ build-static-examples.ts
@@ -1,4 +1,4 @@
-import IntegrationBuilder, { CHAINS, LANGS } from "./src/lib/integration-builder/index";
+import IntegrationBuilder, { CHAINS, LANGS } from "./src/pages/quick-start/integration-builder/index";
 import files from "./files";
 const res = IntegrationBuilder.build({ chain: CHAINS[0].key, lang: LANGS[0].key }, files);
 console.log(res);
