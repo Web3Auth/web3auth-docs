5f2956a4284210f8b19b988e3a078d5aba2a20ef
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-06-07 14:01:30 +0530

    Fix script imports

--- src/pages/quick-start/integration-builder/commonSnippets/get-script-import.ts
+++ src/pages/quick-start/integration-builder/commonSnippets/get-script-import.ts
@@ -1,34 +1,34 @@
 export const getScriptImport = (chain: "eth" | "sol" | "starkex" | "starknet", isWhiteLabled: boolean, isCustomAuth: boolean) => {
   let code = `
-    <script src="https://cdn.jsdelivr.net/npm/@web3auth/web3auth.umd.min.js"></script>`;
+    <script src="https://cdn.jsdelivr.net/npm/@web3auth/web3auth@1.0.0/dist/web3auth.umd.min.js"></script>`;
   if (isWhiteLabled || isCustomAuth) {
     code += `
-    <script src="https://cdn.jsdelivr.net/npm/@web3auth/openlogin-adapter@0/dist/openloginAdapter.umd.min.js"></script>`;
+    <script src="https://cdn.jsdelivr.net/npm/@web3auth/openlogin-adapter@1.0.0/dist/openloginAdapter.umd.min.js"></script>`;
   }
   switch (chain) {
     case "sol":
       code += `
-    <script src="https://cdn.jsdelivr.net/npm/@solana/web3.js@1.43.5/lib/index.iife.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@web3auth/solana-provider@0.9.0/dist/solanaProvider.umd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@solana/web3.js@1.43.5/lib/index.browser.cjs.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@web3auth/solana-provider@1.0.0/dist/solanaProvider.umd.min.js"></script>
     <script src="./solana.js"></script>`;
       break;
     case "starkex":
       code += `
-    <script src="https://cdn.jsdelivr.net/gh/ethereum/web3.js@1/dist/web3.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@toruslabs/starkware-crypto@1.1.0/dist/starkwareCrypto.umd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/web3@1.7.3/lib/index.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@starkware-industries/starkware-crypto-utils@0.0.2/src/js/index.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@starkware-industries/starkex-js@0.0.6/dist/browser.min.js"></script>
     <script src="./starkex.js"></script>`;
       break;
     case "starknet":
       code += `
-    <script src="https://cdn.jsdelivr.net/gh/ethereum/web3.js@1/dist/web3.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@toruslabs/starkware-crypto@1.1.0/dist/starkwareCrypto.umd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/web3@1.7.3/lib/index.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@starkware-industries/starkware-crypto-utils@0.0.2/src/js/index.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/starknet@3.9.0/dist/index.min.js"></script>
     <script src="./starknet.js"></script>`;
       break;
     default:
       code += `
-    <script src="https://cdn.jsdelivr.net/gh/ethereum/web3.js@1/dist/web3.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/web3@1.7.3/lib/index.min.js"></script>
     <script src="./evm.js"></script>`;
       break;
   }
