9e7a667b05d4430ee3d70a6e21064c84f3fcb48f
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-06-27 13:29:18 +0530

    Add tags, change tag to type

--- src/pages/guides/index.tsx
+++ src/pages/guides/index.tsx
@@ -10,7 +10,8 @@ interface Props {
       title: string;
       description: string;
       image: string;
-      tag: string;
+      type: string;
+      tags: string[];
       date: string;
       author: string;
       order: number;
@@ -32,9 +33,20 @@ export default function GuidesPage({ guides }: Props) {
           <Link key={key} className={styles.article} to={`/guides/${key}`}>
             <img src={guide.image} alt="Banner" />
             <div className={styles.contentContainer}>
-              <span className={styles.tag}>{guide.tag}</span>
+              <span className={styles.type}>{guide.type}</span>
               <h3>{guide.title}</h3>
               <p>{guide.description}</p>
+              {guide.tags.map((item, index) => {
+                if (index === guide.tags.length - 1) {
+                  return (
+                    <span>
+                      <code className={styles.tag}>{item}</code>
+                      <br />
+                    </span>
+                  );
+                }
+                return <code className={styles.tag}>{item}</code>;
+              })}
               <span className={styles.date}>
                 {guide.author} | {guide.date}
               </span>
