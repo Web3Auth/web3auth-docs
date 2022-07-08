ca55564ca5a26b657cf0550486fbdd4f5a15f3d7
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-07-08 20:50:22 +0530

    Add dark mode footer

---
+++ src/theme/Footer/footer-dark.jsx
@@ -0,0 +1,180 @@
+import Logo from "@site/static/images/logo-dark.svg";
+import Discord from "@site/static/images/social/discord-dark.svg";
+import GitHub from "@site/static/images/social/github-dark.svg";
+import LinkedIn from "@site/static/images/social/linkedin-dark.svg";
+import Medium from "@site/static/images/social/medium-dark.svg";
+import Telegram from "@site/static/images/social/telegram-dark.svg";
+import Twitter from "@site/static/images/social/twitter-dark.svg";
+import YouTube from "@site/static/images/social/youtube-dark.svg";
+
+export default function FooterComponent() {
+  return (
+    <footer className="footer">
+      <div className="container container--fluid">
+        <div className="row footer__links" style={{ paddingBottom: 82, borderBottomStyle: "solid", borderBottomColor: "#5C6C7F" }}>
+          <div className="col col--3" style={{ padding: 10 }}>
+            <Logo style={{ width: 145 }} />
+            <p>Web3Auth is where passwordless auth meets non-custodial key infrastructure for Web3 apps and wallets.</p>
+            <div style={{ flexDirection: "row" }}>
+              <a href="/docs/api-reference/web/">
+                <Telegram />
+              </a>
+              <a href="/docs/api-reference/web/" style={{ marginLeft: 8 }}>
+                <Twitter />
+              </a>
+              <a href="/docs/api-reference/web/" style={{ marginLeft: 8 }}>
+                <Medium />
+              </a>
+              <a href="/docs/api-reference/web/" style={{ marginLeft: 8 }}>
+                <GitHub />
+              </a>
+              <a href="/docs/api-reference/web/" style={{ marginLeft: 8 }}>
+                <LinkedIn />
+              </a>
+              <a href="/docs/api-reference/web/" style={{ marginLeft: 8 }}>
+                <YouTube />
+              </a>
+              <a href="/docs/api-reference/web/" style={{ marginLeft: 8 }}>
+                <Discord />
+              </a>
+            </div>
+          </div>
+          <div className="col col--1" />
+          <div className="col col--2" style={{ padding: 10 }}>
+            <div className="footer__item">
+              <strong>Product</strong>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs/api-reference/web/">
+                Plug n Play
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs/api-reference/self-host/introduction">
+                Self-hosted
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://calendly.com/web3auth/meeting-with-web3auth" target="_blank" rel="noreferrer">
+                Enterprise Demo
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://web3auth.io/pricing.html" target="_blank" rel="noreferrer">
+                Pricing
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://status.web3auth.io/" target="_blank" rel="noreferrer">
+                System Status
+              </a>
+            </div>
+          </div>
+          <div className="col col--2" style={{ padding: 10 }}>
+            <div className="footer__item">
+              <strong>Developer</strong>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://dashboard.web3auth.io/" target="_blank" rel="noreferrer">
+                Dashboard
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs">
+                Documentation
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs/api-reference">
+                API Reference
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs/guides">
+                Guides
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs/examples">
+                Examples
+              </a>
+            </div>
+          </div>
+          <div className="col col--2" style={{ padding: 10 }}>
+            <div className="footer__item">
+              <strong>Company</strong>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://web3auth.io/about-us.html" target="_blank" rel="noreferrer">
+                About us
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://web3auth.io/partners.html" target="_blank" rel="noreferrer">
+                Partners
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://jobs.lever.co/TorusLabs" target="_blank" rel="noreferrer">
+                Career
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://web3auth.io/media-kit.html" target="_blank" rel="noreferrer">
+                Media Kit
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://web3auth.io/contact-us.html" target="_blank" rel="noreferrer">
+                Contact Us
+              </a>
+            </div>
+          </div>
+          <div className="col col--2" style={{ padding: 10 }}>
+            <div className="footer__item">
+              <strong>Community</strong>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://t.me/web3authdev" target="_blank" rel="noreferrer">
+                Telegram Developer
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="https://discord.gg/web3auth" target="_blank" rel="noreferrer">
+                Discord
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs/contribute">
+                Contribute
+              </a>
+            </div>
+            <div className="footer__item">
+              <a className="footer__link-item" href="/docs/contribute/bug-bounty">
+                Bug Bounty
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col col-6">Copyright © Torus Labs Pte Ltd 2022</div>
+          <div className="col col-3" />
+          <div className="col col-3">
+            {" "}
+            <a className="footer__link-item" href="/docs/legal/cookie-policy">
+              Cookie Policy
+            </a>{" "}
+            |{" "}
+            <a className="footer__link-item" href="/docs/legal/privacy-policy">
+              Privacy Policy
+            </a>{" "}
+            |{" "}
+            <a className="footer__link-item" href="/docs/legal/terms-and-conditions">
+              Terms and Conditions
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,3 +1,14 @@
-import FooterComponent from "./footer";
+import { useColorMode } from "@docusaurus/theme-common";
 
-export default FooterComponent;
+import FooterLight from "./footer-light";
+import FooterDark from "./footer-dark";
+
+export default function FooterComponent() {
+  if (window.location.pathname.startsWith("/docs/integration-builder")) return null;
+  const { isDarkTheme } = useColorMode();
+
+  if (isDarkTheme) {
+    return <FooterDark />;
+  }
+  return <FooterLight />;
+}
