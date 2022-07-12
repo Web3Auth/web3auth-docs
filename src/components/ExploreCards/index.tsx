0d93d2cd48cf90aba3b6c3eac9fa390e72b1910e
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-07-12 17:42:08 +0530

    CSS Changes

--- src/components/ExploreCards/index.tsx
+++ src/components/ExploreCards/index.tsx
@@ -13,42 +13,45 @@ export default function QuickNavigation() {
       </h2>
       <div className={styles.container}>
         <Link className={classNames(styles.card)} to="/docs/integration-builder">
-          <Build className={styles.cardIcon} />
-          <h5 className={styles.title}>
-            <strong>Start Building</strong>
-          </h5>
-          <p>Integrate instantly through our uniquely designed Integration Builder.</p>
           <div>
-            <a className={styles.footer} href="/docs/integration-builder">
-              Explore Integration Builder →
-            </a>
+            <Build className={styles.cardIcon} />
+            <h5 className={styles.title}>
+              <strong>Start Building</strong>
+            </h5>
           </div>
+          <p>Integrate instantly through our uniquely designed Integration Builder.</p>
+
+          <a className={styles.footer} href="/docs/integration-builder">
+            Explore Integration Builder →
+          </a>
         </Link>
 
         <Link className={classNames(styles.card)} to="/docs/whitelabel">
-          <Whitelabel className={styles.cardIcon} />
-          <h5 className={styles.title}>
-            <strong>Whitelabel</strong>
-          </h5>
-          <p>Make Web3Auth your own with a wide range of Whitelabeling options.</p>
           <div>
-            <a className={styles.footer} href="/docs/whitelabel">
-              Checkout Whitelabel Page→
-            </a>
+            <Whitelabel className={styles.cardIcon} />
+            <h5 className={styles.title}>
+              <strong>Whitelabel</strong>
+            </h5>
           </div>
+          <p>Make Web3Auth your own with a wide range of Whitelabeling options.</p>
+
+          <a className={styles.footer} href="/docs/whitelabel">
+            Checkout Whitelabel Page→
+          </a>
         </Link>
 
         <Link className={classNames(styles.card)} to="/docs/custom-authentication/">
-          <CustomAuthentication className={styles.cardIcon} />
-          <h5 className={styles.title}>
-            <strong>Custom Authentication</strong>
-          </h5>
-          <p>Use your own social login and generate a non custodial private key with Web3Auth.</p>
           <div>
-            <a className={styles.footer} href="/docs/custom-authentication/">
-              Turn to Custom Authentication →
-            </a>
+            <CustomAuthentication className={styles.cardIcon} />
+            <h5 className={styles.title}>
+              <strong>Custom Authentication</strong>
+            </h5>
           </div>
+          <p>Use your own social login and generate a non custodial private key with Web3Auth.</p>
+
+          <a className={styles.footer} href="/docs/custom-authentication/">
+            Turn to Custom Authentication →
+          </a>
         </Link>
       </div>
     </div>
