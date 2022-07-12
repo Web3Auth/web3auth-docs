2213686752817937add586ec81b8fd84f58fc154
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-07-12 17:21:57 +0530

    Fixes customers live demo

--- src/components/DemoCards/index.tsx
+++ src/components/DemoCards/index.tsx
@@ -10,12 +10,12 @@ export default function QuickNavigation() {
         <strong>See how others use us</strong>
       </h2>
       <p>
-        Want to experience how our production applications use us? Here are a few links to some of our interesting customer implemetations to help you
-        experience the real world applications of Web3Auth.
+        Want to experience how production applications use us? Here are a few links to some of our exciting customer implementations to help you
+        experience the real-world use-case of Web3Auth.
       </p>
       <p>
-        Most users do not want to be bothered with private keys and mnemonics. Web3Auth abstracts all these complexity behind a smooth Web 2.0
-        experience, allowing users to dive straight into the action.
+        Most users do not want to be bothered with private keys, mnemonics and even seed phrases. Web3Auth abstracts all these complexities behind a
+        smooth Web 2.0 experience, allowing users to dive straight into the action.
       </p>
       <UsecaseCards />
     </div>
