a853c42bcc6e8c7b6c2b3d8584b385b2e1e29c8c
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-07-21 22:58:24 +0530

    Edit integration builder display to add evm frameworks

--- src/pages/integration-builder/builder/steps/index.ts
+++ src/pages/integration-builder/builder/steps/index.ts
@@ -51,6 +51,11 @@ export const TOGGLE_CHOICES: DisplayChoice[] = [
   { key: "yes", displayName: "Yes" },
 ];
 
+export const EVM_FRAMEWORK_CHOICES: DisplayChoice[] = [
+  { key: "web3", displayName: "web3.js" },
+  { key: "ethers", displayName: "ethers" },
+];
+
 export const RN_MODE_CHOICES: DisplayChoice[] = [
   { key: "expo", displayName: "Expo" },
   { key: "bare", displayName: "Bare React Native" },
@@ -61,32 +66,7 @@ const web3authIntegrationBuilder: IntegrationBuilder = {
   displayName: "Web3Auth",
 
   // Options that will be displayed in the UI for selection
-  options: {
-    lang: {
-      displayName: "Language/Framework",
-      default: LANGS[0].key,
-      type: "dropdown",
-      choices: LANGS,
-    },
-    chain: {
-      displayName: "Blockchain",
-      default: CHAINS[0].key,
-      type: "dropdown",
-      choices: CHAINS,
-    },
-    customAuthentication: {
-      displayName: "Custom Authentication",
-      default: TOGGLE_CHOICES[0].key,
-      type: "toggle",
-      choices: TOGGLE_CHOICES,
-    },
-    whitelabel: {
-      displayName: "Whitelabel",
-      default: TOGGLE_CHOICES[0].key,
-      type: "toggle",
-      choices: TOGGLE_CHOICES,
-    },
-  },
+  options: {},
 
   // Build integrations based on input values
   build(values: Record<string, string>, files: Record<string, string>) {
@@ -94,27 +74,73 @@ const web3authIntegrationBuilder: IntegrationBuilder = {
     const filenames: string[] = [];
     const newFiles = JSON.parse(JSON.stringify(files));
     const steps: IntegrationStep[] = [];
+    enum EVM {
+      "eth",
+      "matic",
+      "bnb",
+      "avax",
+      "arbitrum",
+      "optimism",
+      "cronos",
+      "harmony",
+      "celo",
+      "moonbeam",
+      "moonriver",
+    }
+    enum Mobile {
+      "android",
+      "ios",
+      "react-native",
+      "flutter",
+    }
+    this.options = {
+      lang: {
+        displayName: "Language/Framework",
+        default: LANGS[0].key,
+        type: "dropdown",
+        choices: LANGS,
+      },
+    };
+    if (values.lang === "html") {
+      this.options = {
+        ...this.options,
+        chain: {
+          displayName: "Blockchain",
+          default: CHAINS_HTML[0].key,
+          type: "dropdown",
+          choices: CHAINS_HTML,
+        },
+      };
+    } else if (!(values.lang in Mobile)) {
+      this.options = {
+        ...this.options,
+        chain: {
+          displayName: "Blockchain",
+          default: CHAINS[0].key,
+          type: "dropdown",
+          choices: CHAINS,
+        },
+      };
+    }
+    this.options = {
+      ...this.options,
+      customAuthentication: {
+        displayName: "Custom Authentication",
+        default: TOGGLE_CHOICES[0].key,
+        type: "toggle",
+        choices: TOGGLE_CHOICES,
+      },
+      whitelabel: {
+        displayName: "Whitelabel",
+        default: TOGGLE_CHOICES[0].key,
+        type: "toggle",
+        choices: TOGGLE_CHOICES,
+      },
+    };
 
     if (values.lang === "android" || values.lang === "ios" || values.lang === "flutter") {
       this.options = {
-        lang: {
-          displayName: "Language/Framework",
-          default: LANGS[0].key,
-          type: "dropdown",
-          choices: LANGS,
-        },
-        customAuthentication: {
-          displayName: "Custom Authentication",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
-        },
-        whitelabel: {
-          displayName: "Whitelabel",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
-        },
+        ...this.options,
         dynamicConstructorParams: {
           displayName: "Dynamic Constructor Params",
           default: TOGGLE_CHOICES[0].key,
@@ -128,53 +154,9 @@ const web3authIntegrationBuilder: IntegrationBuilder = {
           choices: TOGGLE_CHOICES,
         },
       };
-    } else if (values.lang === "html") {
-      this.options = {
-        lang: {
-          displayName: "Language/Framework",
-          default: LANGS[0].key,
-          type: "dropdown",
-          choices: LANGS,
-        },
-        chain: {
-          displayName: "Blockchain",
-          default: CHAINS_HTML[0].key,
-          type: "dropdown",
-          choices: CHAINS_HTML,
-        },
-        customAuthentication: {
-          displayName: "Custom Authentication",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
-        },
-        whitelabel: {
-          displayName: "Whitelabel",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
-        },
-      };
     } else if (values.lang === "react-native") {
       this.options = {
-        lang: {
-          displayName: "Language/Framework",
-          default: LANGS[0].key,
-          type: "dropdown",
-          choices: LANGS,
-        },
-        customAuthentication: {
-          displayName: "Custom Authentication",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
-        },
-        whitelabel: {
-          displayName: "Whitelabel",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
-        },
+        ...this.options,
         usingEmailPasswordless: {
           displayName: "Using Email Passwordless",
           default: TOGGLE_CHOICES[0].key,
@@ -188,31 +170,31 @@ const web3authIntegrationBuilder: IntegrationBuilder = {
           choices: RN_MODE_CHOICES,
         },
       };
-    } else {
+    }
+
+    this.options = {
+      ...this.options,
+      customAuthentication: {
+        displayName: "Custom Authentication",
+        default: TOGGLE_CHOICES[0].key,
+        type: "toggle",
+        choices: TOGGLE_CHOICES,
+      },
+      whitelabel: {
+        displayName: "Whitelabel",
+        default: TOGGLE_CHOICES[0].key,
+        type: "toggle",
+        choices: TOGGLE_CHOICES,
+      },
+    };
+    if (values.chain in EVM && !(values.lang in Mobile)) {
       this.options = {
-        lang: {
-          displayName: "Language/Framework",
-          default: LANGS[0].key,
+        ...this.options,
+        evmFramework: {
+          displayName: "EVM Chain Framework",
+          default: EVM_FRAMEWORK_CHOICES[0].key,
           type: "dropdown",
-          choices: LANGS,
-        },
-        chain: {
-          displayName: "Blockchain",
-          default: CHAINS[0].key,
-          type: "dropdown",
-          choices: CHAINS,
-        },
-        customAuthentication: {
-          displayName: "Custom Authentication",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
-        },
-        whitelabel: {
-          displayName: "Whitelabel",
-          default: TOGGLE_CHOICES[0].key,
-          type: "toggle",
-          choices: TOGGLE_CHOICES,
+          choices: EVM_FRAMEWORK_CHOICES,
         },
       };
     }
