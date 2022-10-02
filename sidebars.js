47a704652b7fbce73b02c0357d161147ccd40ce7
Saviour1001 <harshghodkar@gmail.com>
2022-10-02 23:09:10 +0530

    added algorand docs

--- sidebars.js
+++ sidebars.js
@@ -83,6 +83,7 @@ module.exports = {
         "connect-blockchain/starkex",
         "connect-blockchain/starknet",
         "connect-blockchain/tezos",
+        "connect-blockchain/algorand",
       ],
     },
     {
