18f5fa9debc46d5702535296073906a5b208b1e9
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-10-29 16:22:51 +0530

    Restructure Android

---
+++ src/pages/integration-builder/builder/steps/frameworks/android/files.ts
@@ -0,0 +1,23 @@
+import { PLACEHOLDERS } from "../../../commonSnippets";
+import { FILENAME_MAINACTIVITY } from "./filenames";
+import { getConstructorCodeAndroid, getLoginCodeAndroid } from "./replacementCode";
+
+export default function getUpdatedFiles(files, whitelabel, customAuthentication, mfa, replacementAggregator) {
+  const newFiles = files;
+
+  const ConstructorCodeAndroid = getConstructorCodeAndroid(whitelabel === "yes", customAuthentication === "yes");
+  newFiles[FILENAME_MAINACTIVITY] = replacementAggregator.replaceFileVariable(
+    files[FILENAME_MAINACTIVITY],
+    FILENAME_MAINACTIVITY,
+    PLACEHOLDERS.CONSTRUCTOR_CODE,
+    ConstructorCodeAndroid
+  );
+
+  const LoginCodeAndroid = getLoginCodeAndroid(mfa === "yes");
+  newFiles[FILENAME_MAINACTIVITY] = replacementAggregator.replaceFileVariable(
+    files[FILENAME_MAINACTIVITY],
+    FILENAME_MAINACTIVITY,
+    PLACEHOLDERS.ANDROID_LOGIN_CONFIG,
+    LoginCodeAndroid
+  );
+}
