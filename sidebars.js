041b754f0643110606cfc523f8c90cc0ec89f6c5
Saviour1001 <harshghodkar@gmail.com>
2022-12-07 23:25:05 +0530

    fixes

--- sidebars.js
+++ sidebars.js
@@ -191,7 +191,7 @@ module.exports = {
       Troubleshooting: [
         "troubleshooting/error-429",
         "troubleshooting/webpack-issues",
-        "troubleshooting/nodejs-polyfill-issues",
+        "troubleshooting/bundler-polyfill-issues",
         "troubleshooting/jwt-errors",
         "troubleshooting/different-wallet-address-issue",
         "troubleshooting/react-big-int-error",
