3e0c22a5db67ee13e90fe4d53be7322dbc61ece6
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-12-16 06:35:34 +0530

    Add Android + iOS files and requirements

--- src/pages/integration-builder/builder/flutter/filenames.ts
+++ src/pages/integration-builder/builder/flutter/filenames.ts
@@ -1,5 +1,7 @@
 export const FILENAME_MAINACTIVITY = "frameworks/flutter/main.dart";
+export const FILENAME_BUILDGRADLE = "frameworks/flutter/build.gradle";
+export const FILENAME_PODFILE = "frameworks/flutter/Podfile";
 
 export default function getFileNames(filenames) {
-  filenames.push(FILENAME_MAINACTIVITY);
+  filenames.push(FILENAME_MAINACTIVITY, FILENAME_BUILDGRADLE, FILENAME_PODFILE);
 }
