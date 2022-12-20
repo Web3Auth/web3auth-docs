bf5e3fd272980daa0f620f8d57163e5d0f10779d
Saviour1001 <harshghodkar@gmail.com>
2022-12-20 12:30:09 +0800

    iOS done :tada:

--- src/pages/integration-builder/builder/ios/index.ts
+++ src/pages/integration-builder/builder/ios/index.ts
@@ -1,13 +1,15 @@
 import { ReplaceFileAggregator } from "../../utils";
 import getFileNames from "./filenames";
+import getUpdatedFiles from "./files";
 import getSteps from "./steps";
 // For iOS SDK, dynamicConstructorParams must be YES if whitelabel is YES or customAuth == YES.
 // Will also need the ability to have whitelabel and customAuth being YES in the same time as well.
 
 const framework = {
-  build({ filenames, files, steps, whitelabel, customAuth }) {
+  build({ chain, filenames, files, steps, whitelabel, customAuth, mfa }) {
     const replacementAggregator = new ReplaceFileAggregator();
-    getSteps(steps, files, replacementAggregator, whitelabel, customAuth);
+    getUpdatedFiles(chain, files, whitelabel, customAuth, mfa, replacementAggregator);
+    getSteps(steps, files, replacementAggregator, whitelabel, customAuth, mfa);
     getFileNames(filenames);
 
     return { filenames, files, steps };
