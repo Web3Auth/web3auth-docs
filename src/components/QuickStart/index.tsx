f0db999c76f9ab3da8fbdd76fb89946ccfaea75a
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2023-02-21 14:07:27 +0530

    nit

--- src/components/QuickStart/index.tsx
+++ src/components/QuickStart/index.tsx
@@ -111,7 +111,7 @@ export default function QuickNavigation() {
 
   return (
     <div>
-      <h2 className={styles.heading}>1. Select the Web3Auth product you would like to build upon</h2>
+      <h2 className={styles.heading}>1. Select the Web3Auth Product you would like to build upon</h2>
       <div className={styles.container}>
         <div className={product === pnp ? styles.selectedCard : styles.card} onClick={() => changeProduct(pnp)}>
           <div>
@@ -148,7 +148,7 @@ export default function QuickNavigation() {
           <p>Build on top of the Web3Auth infrastructural layer and build your own UX flows.</p>
         </div>
       </div>
-      <h2 className={styles.heading}>2. Next, Select which SDK and platform you intend to use</h2>
+      <h2 className={styles.heading}>2. Select which SDK and platform you intend to use</h2>
       <div className={styles.container}>
         <div className={styles.list}>
           <h3>Select which SDK to use</h3>
