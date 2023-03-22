5bc2e911617491f8eea96f06343ef7733f848a0b
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2023-03-22 12:43:21 +0530

    Content Discussion Comment

---
+++ src/components/DiscourseComment/index.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export default function DiscourseComment(props) {
+  // eslint-disable-next-line react/prop-types
+  const { topicId } = props;
+  useEffect(() => {
+    window.DiscourseEmbed = {
+      discourseUrl: "https://community.web3auth.io/",
+      topicId,
+    };
+
+    const d = document.createElement("script");
+    d.type = "text/javascript";
+    d.async = true;
+    d.src = `${window.DiscourseEmbed.discourseUrl}javascripts/embed.js`;
+    (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(d);
+  }, []);
+
+  return <div id="discourse-comments" />;
+}
