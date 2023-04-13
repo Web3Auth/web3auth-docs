b585da2c2645e715fc61b9d1028a28a79292d132
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2023-04-14 00:41:40 +0530

    Add SFA Android to home page + quick start

--- src/components/SDKReferenceCards/index.tsx
+++ src/components/SDKReferenceCards/index.tsx
@@ -282,6 +282,27 @@ export default function QuickNavigation() {
         </div>
         <div className={styles.cardContentText}>
           <div className={styles.cardTitleContainer}>
+            <h3 className={styles.title}>Single Factor Auth Android SDK</h3>
+          </div>
+          <p>Core Kit Single Factor Auth Android SDK for your Android application. Implement Core Kit in a single key pair flow</p>
+          <div className={styles.links}>
+            <a href="/docs/quick-start?product=Core+Kit&sdk=Single+Factor+Auth+Android+SDK&platform=Android">Quick Start{chevron}</a>
+            <a href="/docs/sdk/single-factor-auth-android/">SDK Reference{chevron}</a>
+            <a href="/docs/examples?product=Core+Kit&sdk=Single+Factor+Auth+Android+SDK">Examples{chevron}</a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.cardContent}>
+        <div className={styles.sdkIconContainer}>
+          <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className={styles.sdkIcon}>
+            <path
+              d="M420.55,301.93a24,24,0,1,1,24-24,24,24,0,0,1-24,24m-265.1,0a24,24,0,1,1,24-24,24,24,0,0,1-24,24m273.7-144.48,47.94-83a10,10,0,1,0-17.27-10h0l-48.54,84.07a301.25,301.25,0,0,0-246.56,0L116.18,64.45a10,10,0,1,0-17.27,10h0l47.94,83C64.53,202.22,8.24,285.55,0,384H576c-8.24-98.45-64.54-181.78-146.85-226.55"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
+        <div className={styles.cardContentText}>
+          <div className={styles.cardTitleContainer}>
             <h3 className={styles.title}>tKey Android SDK</h3>
             <div className={styles.pillContainer}>
               <div className={styles.pill}>Coming Soon</div>
