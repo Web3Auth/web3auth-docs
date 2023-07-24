d5a46718058a9c66bbf1e545845cf9257c253df6
Maharshi Mishra <ihsraham27@gmail.com>
2023-07-25 01:44:33 +0530

    Made changes as per the PR review comments

--- sidebars.js
+++ sidebars.js
@@ -148,7 +148,6 @@ module.exports = {
         "dashboard-setup/setup-custom-authentication",
         "dashboard-setup/billing-and-user-stats",
         "dashboard-setup/roles-and-permissions",
-        "dashboard-setup/getting-the-same-private-key",
       ],
     },
     {
@@ -245,6 +244,7 @@ module.exports = {
       label: "Troubleshooting",
       link: { type: "doc", id: "troubleshooting/troubleshooting" },
       items: [
+        "troubleshooting/diff-private-key-across-integn",
         "troubleshooting/sdk-errors-warnings",
         "troubleshooting/error-429",
         "troubleshooting/webpack-issues",
