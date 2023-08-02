4afdfa5995d3c0b40e00d352a40cface130788c7
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2023-08-03 02:13:37 +0530

    Update SDK Reference Cards

--- src/components/SDKReferenceCards/index.tsx
+++ src/components/SDKReferenceCards/index.tsx
@@ -301,9 +301,9 @@ export default function QuickNavigation() {
       <div className={styles.cardContainerIntro}>
         <div className={styles.cardTitleContainer}>
           <h3 className={styles.title}>Gaming SDKs</h3>
-          <div className={styles.pillContainer}>
+          {/* <div className={styles.pillContainer}>
             <div className={styles.pill}>{pnp} Only</div>
-          </div>
+          </div> */}
         </div>
 
         <p>Seamlessly authenticate users into your Web3 games with their socials using Web3Auth Gaming SDKs.</p>
@@ -628,6 +628,27 @@ export default function QuickNavigation() {
               <a href={`${baseUrl}quick-start?product=Core+Kit&sdk=tKey+JS+SDK&platform=React+Native`}>Quick Start{chevron}</a>
               <a href={`${baseUrl}sdk/core-kit/tkey-react-native`}>SDK Reference{chevron}</a>
               <a href={`${baseUrl}examples?product=Core+Kit&sdk=tKey+JS+SDK`}>Examples{chevron}</a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.cardContent}>
+          <div className={styles.sdkIconContainer}>
+            <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className={styles.sdkIcon}>
+              <path
+                d="M420.55,301.93a24,24,0,1,1,24-24,24,24,0,0,1-24,24m-265.1,0a24,24,0,1,1,24-24,24,24,0,0,1-24,24m273.7-144.48,47.94-83a10,10,0,1,0-17.27-10h0l-48.54,84.07a301.25,301.25,0,0,0-246.56,0L116.18,64.45a10,10,0,1,0-17.27,10h0l47.94,83C64.53,202.22,8.24,285.55,0,384H576c-8.24-98.45-64.54-181.78-146.85-226.55"
+                fill="currentColor"
+              />
+            </svg>
+          </div>
+          <div className={styles.cardContentText}>
+            <div className={styles.cardTitleContainer}>
+              <h3 className={styles.title}>tKey Android SDK</h3>
+            </div>
+            <p>Core Kit tKey Native SDK for Android Platform</p>
+            <div className={styles.links}>
+              <a href={`${baseUrl}quick-start?product=Core+Kit&sdk=tKey+Android+SDK&platform=Android`}>Quick Start{chevron}</a>
+              <a href={`${baseUrl}sdk/core-kit/tkey-android`}>SDK Reference{chevron}</a>
+              <a href={`${baseUrl}examples?product=Core+Kit&sdk=tKey+Android+SDK`}>Examples{chevron}</a>
             </div>
           </div>
         </div>
