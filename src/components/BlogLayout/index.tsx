fa52b4091bb45c6deceeabf35cb234e7f96c7085
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2023-10-10 22:11:26 +0530

    Update SEO Issues

--- src/components/BlogLayout/index.tsx
+++ src/components/BlogLayout/index.tsx
@@ -1,8 +1,6 @@
-import { PageMetadata } from "@docusaurus/theme-common";
 import Layout from "@theme/Layout";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 
-import SEO from "../SEO";
 import BlogDetail from "./[slug]";
 import BlogNotFound from "./BlogNotFound";
 
@@ -10,29 +8,17 @@ function BookLayout() {
   const match = useRouteMatch();
 
   return (
-    <>
-      <SEO
-        title="Web3Auth Blogs"
-        description="Blogs - Web3Auth Content Hub"
-        image="http://web3auth.io/docs/images/docs-meta-cards/documentation-card.png"
-      />
-      <PageMetadata
-        title="Web3Auth Blogs"
-        description="Blogs - Web3Auth Content Hub"
-        image="http://web3auth.io/docs/images/docs-meta-cards/documentation-card.png"
-      />
-      <Switch>
-        <Route exact path={`${match.path}:slug`}>
-          <Layout title="Blog detail">
-            <BlogDetail />
-          </Layout>
-        </Route>
+    <Switch>
+      <Route exact path={`${match.path}:slug`}>
+        <Layout title="Blog detail">
+          <BlogDetail />
+        </Layout>
+      </Route>
 
-        <Route>
-          <BlogNotFound />
-        </Route>
-      </Switch>
-    </>
+      <Route>
+        <BlogNotFound />
+      </Route>
+    </Switch>
   );
 }
 
