9a598423f559fe5ca6d80d08a55e6ba49037395c
Yashovardhan Agrawal <yash@web3auth.io>
2023-10-11 13:13:59 +0530

    Dynamic Page SEO changes

--- plugins/plugin-dynamic-route/index.js
+++ plugins/plugin-dynamic-route/index.js
@@ -1,18 +1,18 @@
 // Load content dynamically
 module.exports = async (context, options) => {
-    return {
-        name: 'plugin-dynamic-route',
-        async loadContent() {
-            /* ... */
-        },
-        async contentLoaded({ content, actions }) {
-            const { routes } = options;
-            const { addRoute } = actions;
+  return {
+    name: "plugin-dynamic-route",
+    async loadContent() {
+      /* ... */
+    },
+    async contentLoaded({ content, actions }) {
+      const { routes } = options;
+      const { addRoute } = actions;
 
-            routes.forEach((route) => {
-                // console.log('Adding route', route)
-                addRoute(route)
-            });
-        },
-    };
+      routes.forEach((route) => {
+        // console.log('Adding route', route)
+        addRoute(route, content);
+      });
+    },
+  };
 };
