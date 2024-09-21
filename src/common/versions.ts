4a469de9d7522c28cc890251b571e7ec4a0581ef
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-09-21 18:03:48 +0530

    update version

--- src/common/versions.ts
+++ src/common/versions.ts
@@ -7,7 +7,7 @@ export const pnpUnityVersion = `5.x.x`;
 export const pnpUnrealVersion = `4.1.x`;
 
 export const sfaWebVersion = `8.0.x`;
-export const sfaAndroidVersion = `0.0.6`;
+export const sfaAndroidVersion = `1.1.0`;
 export const sfaIOSVersion = `8.0.0`;
 export const sfaRNVersion = `2.0.x`;
 export const sfaFlutterVersion = `5.1.0`;
