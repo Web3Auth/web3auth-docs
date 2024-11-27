9573ddeebce5d7433a46635a609964b7de362ceb
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-11-27 16:05:29 +0530

    Add Ecosystem Wallets

--- sidebars.ts
+++ sidebars.ts
@@ -299,6 +299,7 @@ const sidebars: SidebarsConfig = {
     "features/account-dashboard",
     "features/blockchain-agnostic",
     "features/custom-authentication",
+    "features/ecosystem-wallets",
     "features/wallet-ui",
     "features/topup",
     "features/gaming",
