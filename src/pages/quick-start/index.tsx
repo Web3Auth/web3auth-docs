4ac8a905150ec16fea6eff718817e2937edbcdd8
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2025-02-14 10:45:23 +0530

    nit

--- src/pages/quick-start/index.tsx
+++ src/pages/quick-start/index.tsx
@@ -405,10 +405,7 @@ export default function IntegrationBuilderPage({ files }: { files: Record<string
                 {integration.embedLink && (
                   <button
                     className={styles.previewButton}
-                    onClick={() => {
-                      togglePreviewModal(integration.embedLink);
-                      setAbortCountdown(true);
-                    }}
+                    onClick={() => togglePreviewModal(integration.embedLink)}
                     type="button"
                     id="headerButton"
                   >
@@ -417,10 +414,7 @@ export default function IntegrationBuilderPage({ files }: { files: Record<string
                 )}
                 <button
                   className={styles.copyButton}
-                  onClick={() => {
-                    window.open(integration.sourceCodeLink, "_blank");
-                    setAbortCountdown(true);
-                  }}
+                  onClick={() => window.open(integration.sourceCodeLink, "_blank")}
                   type="button"
                   id="headerButton"
                 >
