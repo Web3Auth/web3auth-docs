18cc8e0fbe43340ffa045c43f53c700a92e64bff
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-05-08 12:47:13 +0530

    Update the env variable plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -246,7 +246,17 @@ const config: Config = {
       { rootDir: ".integrationBuilderCache" },
     ],
     path.resolve(__dirname, "plugins", "node-polyfills"),
-    path.resolve(__dirname, "plugins", "env-variables-plugin"),
+    [
+      "docusaurus2-dotenv",
+      {
+        path: "./.env", // The path to your environment variables.
+        safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
+        systemvars: false, // Set to true if you would rather load all system variables as well (useful for CI purposes)
+        silent: false, //  If true, all warnings will be suppressed
+        expand: false, // Allows your variables to be "expanded" for reusability within your .env file
+        defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
+      },
+    ],
     [
       "@docusaurus/plugin-client-redirects",
       {
