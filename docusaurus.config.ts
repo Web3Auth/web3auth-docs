6c4e1d9bba31f25d21f547fb1f69272ccea2da3f
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-05-28 15:47:39 +0530

    Update the module with proxy server

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -246,7 +246,6 @@ const config: Config = {
       { rootDir: ".integrationBuilderCache" },
     ],
     path.resolve(__dirname, "plugins", "node-polyfills"),
-    path.resolve(__dirname, "plugins", "env-variables-plugin"),
     [
       "@docusaurus/plugin-client-redirects",
       {
